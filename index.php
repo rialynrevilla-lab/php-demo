bfc92f07f2ed559300bdd83c5df067cedd17adc4
Rialyn <rialyn.revilla@cvsu.edu.ph>
2026-04-23 19:26:56 +0800

    feat: added index.php

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+         body {
+            background-color: #6acdde;
+            color: #1a1817;
+            font-family: Georgia, serif;
+            font-size: 20px;
+        }
+
+        h1 {
+            font-size: 40px;
+        }
+    </style>
+</head>
+<body>
+    <h1> Laboratories</h1>
+
+    <a href="lab1.php">Laboratory 1</a><br><br>
+    <a href="lab2.php">Laboratory 2</a><br><br>
+    <a href="lab3.php">Laboratory 3</a><br><br>
+</body>
+</html>
